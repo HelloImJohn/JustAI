a5e3b23e65e6431e773a7bb0109686f7b4dfa32b
John Suckau <HeyImActuallyJohn@gmail.com>
2021-09-21 14:13:10 +0200

    Update Interference.py

--- Interference.py
+++ Interference.py
@@ -5,51 +5,53 @@ from matplotlib import pyplot as plt
 import math
 from matplotlib.pyplot import figure
 
-#var bestimmung..
+#var definition..
 start = 0
 stop = 100 #cm
 #length of board that light falls on to..
 length = stop - start
 
 distGridBoard = 100 #cm
-#spaltGr..
+#slitSize..
 b = 0.1 #cm
-#spalt abstand..
+#slit distance..
 d = 1 #cm
 #wave length..
 w = 550 #nm
 
-grad = 360
-
 figure(figsize=(6, 4), dpi=100)
 
-#anzahl der Lichtstrahle (adl) pro spalt..
-adl = 100
-#anzahl der Spalte..
-spalte = 2
+#amount of light rays per slit..
+lps = 100
+#amount of slits..
+slits = 2
 
-#y values..
-y = np.arange(start, stop, length /adl)
-#(spalte, light rays, length, dist)
+
+#(slits, light rays, length, dist)
 def calc(s, lr, l, d, wl):
   for i in range(s - 1):
     for j in range(lr):
-      p = j * (l / lr)
       m = l / 2
-      lIntens = np.tan((p - (m + d + (b / 2))) / d) - np.tan(p - (m - d - (b / 2)) / d)
-      y[j] = lIntens
-      print(lIntens)
+      distCenter = abs(m - j + i * d)
+      #lIntens = 
+      #y values..
+      y = np.arange(start, stop, length /lps)
+      x = np.arange(start, stop, length /lps)   # start,stop,step..
+      y[(i+1)*j] = distCenter
+      #print(lIntens)
 
-calc(spalte, adl, length, d, w)
+      plt.plot(x,y)
+
+calc(slits, lps, length, d, w)
 
 
 
 
 
 
-x = np.arange(start, stop, length /adl)   # start,stop,step..
+
 #y = np.sin(x)
 
 
-plt.plot(x,y)
+
 plt.show()
